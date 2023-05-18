7bac6d53935628d77efe42e0d96e4d8c71cbc78f
Jeeseob <jeeseob5761@gmail.com>
2023-05-18 17:15:23 +0900

    update pom

--- benchmark_run.sh
+++ benchmark_run.sh
@@ -1,2 +1,2 @@
-./bin/spark-submit --class edu.sogang.benchmark.RunBench benchmarks/target/benchmarks-1.0.jar \
+/opt/spark/bin/spark-submit --class edu.sogang.benchmark.RunBench benchmarks/target/benchmarks-1.0.jar \
   --query-name q1 --config-filename benchmarks/src/main/resources/config.properties
